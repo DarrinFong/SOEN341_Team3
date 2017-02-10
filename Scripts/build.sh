eead5a3d2795530570e4f7f08531df458dc8bc2f
Christina Nguyen <ChristinaThao@users.noreply.github.com>
2017-02-09 22:39:30 -0500

    Specification to project

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,37 +3,39 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
+# pwd is soen341
 
-echo "Attempting to build $project for Windows"
+project="Coding_Game"
+
+echo "Attempting to build Coding_Game for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -logFile soen341/unity.log \
+  -projectPath soen341 \
+  -buildWindowsPlayer "soen341/Build/windows/Coding_Game.exe" \
   -quit
 
-echo "Attempting to build $project for OS X"
+echo "Attempting to build Coding_Game for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -logFile soen341/unity.log \
+  -projectPath soen341 \
+  -buildOSXUniversalPlayer "soen341/Build/osx/Coding_Game.app" \
   -quit
 
-echo "Attempting to build $project for Linux"
+echo "Attempting to build Coding_Game for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+  -logFile soen341/unity.log \
+  -projectPath soen341 \
+  -buildLinuxUniversalPlayer "soen341/Build/linux/Coding_Game.exe" \
   -quit
 
 echo 'Logs from build'
-cat $(pwd)/unity.log
+cat soen341/unity.log
