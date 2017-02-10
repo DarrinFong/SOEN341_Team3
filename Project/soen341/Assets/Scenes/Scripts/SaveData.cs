809e647ad5cf486e9706e40977ff199bf32fed85
claudiaconfalone <confaloneclaudia@gmail.com>
2017-02-10 12:07:14 -0500

    Save Load Menu
    Added saving/loading functionality and basic menu.

---
+++ Project/soen341/Assets/Scenes/Scripts/SaveData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+
+    public static SaveData current;
+    public string saveName;
+
+    public SaveData(string saveName)
+    {
+        this.saveName = saveName;
+    
+    }
+}
---
+++ Project/soen341/Assets/Scenes/Scripts/SaveLoad.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLoad : MonoBehaviour {
+
+     public void NewGame(InputField name)
+     {
+        SaveData.current = new SaveData(name.text);
+        Debug.Log(SaveData.current.saveName);
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, SaveData.current);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            SaveData.current = (SaveData)bf.Deserialize(file);
+            Debug.Log(SaveData.current.saveName);
+            file.Close();
+
+        }
+    }
+}
