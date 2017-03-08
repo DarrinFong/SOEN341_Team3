052e259901b4688885a8951a52d52bacee7de012
Christina Nguyen <christina.thao.nguyen@hotmail.com>
2017-03-08 13:45:27 -0500

    Move backwards
    commented out left since it does not fully work yet

---
+++ Project/soen341/Assets/Scenes/Scripts/CharacterLeft.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterLeft : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var leftButton = transform.GetComponent<Button>();
+        leftButton.GetComponent<Button>().onClick.AddListener(delegate {
+            clicked();
+        });
+    }
+
+    public void clicked()
+    {
+        print("left");
+        CharacterActions charController = GameObject.FindObjectOfType<CharacterActions>();
+        charController.left();
+    }
+}
