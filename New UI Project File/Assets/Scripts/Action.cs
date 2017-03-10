f6807e82b9d788ec7a10d6693a58273e414bd2e4
aagalati <galati_ator@msn.com>
2017-03-10 10:13:08 -0500

    Updated UI with new panel config, loops, ifs, action and character blocks

--- New UI Project File/Assets/Scripts/Action.cs
+++ New UI Project File/Assets/Scripts/Action.cs
@@ -6,15 +6,64 @@ public class Action : Block {
 
     public Transform shape;
 
+    enum Type {forward, backward, left, right}
+
 	// Use this for initialization
 	void Start () {
         size = new Vector3(shape.localScale.x, shape.localScale.y, 0);
-        print("size of action block " + size);
+        //print("size of action block " + size);
 	}
-	
+
+    public override IEnumerator executeBlock()
+    {
+        switch (type)
+        {
+            case 0:
+                print("Forward");
+                break;
+            case 1:
+                print("Backward");
+                break;
+            case 2:
+                print("Left");
+                break;
+            case 3:
+                print("Right");
+                break;
+            default:
+                yield return new WaitForSeconds(0.01f);
+                print("Text not set");
+                break;
+        }
+        
+    }
+
+    public override void changeType()
+    {
+
+        switch (type)
+        {
+            case 0:
+                setText("Forward");
+                break;
+            case 1:
+                setText("Backward");
+                break;
+            case 2:
+                setText("Left");
+                break;
+            case 3:
+                setText("Right");
+                break;
+            default:
+                setText("Text not set");
+                break;
+        }
+
+    }
+
 	// Update is called once per frame
 	//void Update () {
 		
 	//}
-
 }
