cd70a7925851459b08e332c6b8f9b30bfcf65e36
joeyfallu <josephfallu@gmail.com>
2017-04-11 22:46:38 -0400

    added some unit tests

---
+++ Project/Assembly-CSharpTests2/Assets/Scenes/Scripts/LogOnInterfaceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogOnInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogOnInterface.Tests
+{
+    [TestClass()]
+    public class LogOnInterfaceTests
+    {
+        [TestMethod()]
+        public void checkpassTest()
+        {
+            LogOnInterface test = new LogOnInterface();
+            bool t1 = test.checkpass("soen341");
+            bool t2 = test.checkpass("SOEN341");
+            bool t3 = test.checkpass("Soen341");
+            bool t4 = test.checkpass("wofiwefoiweh22434234f");
+            Assert.IsTrue(t1);
+            Assert.IsFalse(t2);
+            Assert.IsFalse(t3);
+            Assert.IsFalse(t4);
+        }
+    }
+}
---
+++ Project/Assembly-CSharpTests2/Assets/Scenes/Scripts/SaveLoadInterfaceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaveLoadInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveLoadInterface.Tests
+{
+    [TestClass()]
+    public class SaveLoadInterfaceTests
+    {
+        SaveLoadInterface test = new SaveLoadInterface();
+        [TestMethod()]
+        public void ValidateUsernameTest()
+        {
+            
+            bool t1 = test.ValidateUsername("bob");
+            bool t2 = test.ValidateUsername("bo123 5 235b");
+            bool t3 = test.ValidateUsername("_!@#$");
+            Assert.IsTrue(t1);
+            Assert.IsFalse(t2);
+            Assert.IsFalse(t3);
+        }
+
+        [TestMethod()]
+        public void CheckPasswordsTest()
+        {
+            bool t1 = test.CheckPasswords("bob123","bob123");
+            Assert.IsTrue(t1);
+        }
+    }
+}
