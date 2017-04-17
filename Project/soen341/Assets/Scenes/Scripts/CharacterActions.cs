be2ec48c22ae0642c97c63168c15a7b716db50cd
Darrin Fong <darrinfong@gmail.com>
2017-04-17 19:46:25 -0400

    changed refereces to 'Male/Female' and 'MaleModel/FemaleModel'

--- Project/soen341/Assets/Scenes/Scripts/CharacterActions.cs
+++ Project/soen341/Assets/Scenes/Scripts/CharacterActions.cs
@@ -19,11 +19,11 @@ public class CharacterActions : MonoBehaviour {
     private void Start()
     {
         male = SaveData.current.active.gender != 1;
-        femaleAvatar = GameObject.Find("Girl");
-		maleAvatar = GameObject.Find("C_man_1_FBX2013");
+        femaleAvatar = GameObject.Find("FemaleModel");
+		maleAvatar = GameObject.Find("MaleModel");
 		maleAvatar.GetComponent<Renderer>().enabled = male;
 		femaleAvatar.GetComponent<Renderer>().enabled = !male;
-        Physics.IgnoreCollision(GameObject.Find("DickButt").GetComponent<SphereCollider>(), GameObject.Find("girl").GetComponent<SphereCollider>());
+        Physics.IgnoreCollision(GameObject.Find("Male").GetComponent<SphereCollider>(), GameObject.Find("Female").GetComponent<SphereCollider>());
     }
 
     public void setMale(bool male)
@@ -35,21 +35,21 @@ public class CharacterActions : MonoBehaviour {
     
     public void right()
     {
-        print("dickbutt right, pointer: " + actionPointer);
+		print("Character right, pointer: " + actionPointer);
         actionSequence[actionPointer] = 'r';
         actionPointer++;
     }
 
     public void left()
     {
-        print("dickbutt left, pointer: " + actionPointer);
+		print("Character left, pointer: " + actionPointer);
         actionSequence[actionPointer] = 'l';
         actionPointer++;
     }
 
     public void forward()
     {
-        print("dickbutt forward, pointer: " + actionPointer);
+        print("Character forward, pointer: " + actionPointer);
         actionSequence[actionPointer] = 'f';
         actionPointer++;
 
@@ -57,7 +57,7 @@ public class CharacterActions : MonoBehaviour {
 
     public void backward()
     {
-        print("dickbutt backward, pointer: " + actionPointer);
+		print("Character backward, pointer: " + actionPointer);
         actionSequence[actionPointer] = 'b';
         actionPointer++;
     }
@@ -75,8 +75,8 @@ public class CharacterActions : MonoBehaviour {
     
     public System.Collections.IEnumerator runActions(int Whatever)
     {
-        SimpleCharacterControlGirl girl = GameObject.FindObjectOfType<SimpleCharacterControlGirl>();
-        SimpleCharacterControl dickButt = GameObject.FindObjectOfType<SimpleCharacterControl>();
+        SimpleCharacterControlGirl femaleCharacter = GameObject.FindObjectOfType<SimpleCharacterControlGirl>();
+        SimpleCharacterControl maleCharacter = GameObject.FindObjectOfType<SimpleCharacterControl>();
         levelDestination = GameObject.FindObjectOfType<Destination>();
 
         // m_animator.SetTrigger("Jump");
@@ -93,74 +93,74 @@ public class CharacterActions : MonoBehaviour {
                 case 'f':
                     while (System.Math.Abs(positionBeforeAction.x - transform.position.x) < 0.9889f && System.Math.Abs(positionBeforeAction.z - transform.position.z) < 0.9889f)
                     {
-                        dickButt.setV(1.0f);
-                        girl.setV(1.0f);
+					maleCharacter.setV(1.0f);
+					femaleCharacter.setV(1.0f);
                         yield return new WaitForSeconds(0);
                     }
-                    dickButt.setV(0.0f);
-                    girl.setV(0.0f);
+					maleCharacter.setV(0.0f);
+					femaleCharacter.setV(0.0f);
                     print("position - x: " + (float)(System.Math.Round(transform.position.x, 0)) + ", z: " + (float)(System.Math.Round(transform.position.z, 0)));
-                    girl.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
-                    dickButt.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
+					femaleCharacter.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
+					maleCharacter.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
                     break;
                 case 'b':
                     while (System.Math.Abs(positionBeforeAction.x - transform.position.x) < 0.9889f && System.Math.Abs(positionBeforeAction.z - transform.position.z) < 0.9889f)
                     {
-                        dickButt.setV(-1.0f);
-                        girl.setV(-1.0f);
+						maleCharacter.setV(-1.0f);
+						femaleCharacter.setV(-1.0f);
                         yield return new WaitForSeconds(0);
                     }
-                    dickButt.setV(0.0f);
-                    girl.setV(0.0f);
+					maleCharacter.setV(0.0f);
+					femaleCharacter.setV(0.0f);
                     print("position - x: " + (float)(System.Math.Round(transform.position.x, 0)) + ", z: " + (float)(System.Math.Round(transform.position.z, 0)));
-                    girl.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
-                    dickButt.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
+					femaleCharacter.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
+					maleCharacter.transform.position = new Vector3((float)(System.Math.Round(transform.position.x, 0)), -0.5f, (float)(System.Math.Round(transform.position.z, 0)));
                     break;
                 case 'r':
                     while (System.Math.Abs(transform.eulerAngles.y - rotationBeforeAction.y) < 89)
                     {
-                        dickButt.setH(1.0f);
-                        girl.setH(1.0f);
+						maleCharacter.setH(1.0f);
+						femaleCharacter.setH(1.0f);
                         yield return new WaitForSeconds(0);
                     }
-                    dickButt.setH(0.0f);
-                    girl.setH(0.0f);
+					maleCharacter.setH(0.0f);
+					femaleCharacter.setH(0.0f);
                     print("angle: " + (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100));
-                    girl.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
-                    dickButt.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
+					femaleCharacter.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
+					maleCharacter.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
                     break;
                 case 'l':
                     if (System.Math.Round(rotationBeforeAction.y, 1) == 0)
                         while (System.Math.Abs(System.Math.Round(transform.eulerAngles.y, 1) - System.Math.Round(rotationBeforeAction.y, 1)) > 271 || (System.Math.Round(transform.eulerAngles.y, 1) == 0))
                         {
-                            dickButt.setH(-1.0f);
-                            girl.setH(-1.0f);
+							maleCharacter.setH(-1.0f);
+							femaleCharacter.setH(-1.0f);
                             yield return new WaitForSeconds(0);
                         }
                     else
                         while (System.Math.Abs(transform.eulerAngles.y - rotationBeforeAction.y) < 89)
                         {
-                            dickButt.setH(-1.0f);
-                            girl.setH(-1.0f);
+							maleCharacter.setH(-1.0f);
+							femaleCharacter.setH(-1.0f);
                             yield return new WaitForSeconds(0);
                         }
-                    dickButt.setH(0.0f);
-                    girl.setH(0.0f);
+					maleCharacter.setH(0.0f);
+					femaleCharacter.setH(0.0f);
                     print("angle: " + (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100));
-                    girl.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
-                    dickButt.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
+					femaleCharacter.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
+					maleCharacter.transform.eulerAngles = new Vector3(0, (float)(System.Math.Round(transform.eulerAngles.y / 100, 1) * 100), 0);
                     break;
                 default:
-                    dickButt.setV(0.0f);
-                    dickButt.setH(0.0f);
-                    girl.setV(0.0f);
-                    girl.setH(0.0f);
+					maleCharacter.setV(0.0f);
+					maleCharacter.setH(0.0f);
+					femaleCharacter.setV(0.0f);
+					femaleCharacter.setH(0.0f);
                     break;
             }
-            dickButt.setV(0.0f);
-            dickButt.setH(0.0f);
-            girl.setV(0.0f);
-            girl.setH(0.0f);
+			maleCharacter.setV(0.0f);
+			maleCharacter.setH(0.0f);
+			femaleCharacter.setV(0.0f);
+			femaleCharacter.setH(0.0f);
         }
         yield return new WaitForSeconds(1);
         
@@ -168,10 +168,10 @@ public class CharacterActions : MonoBehaviour {
         if (isDestinationReached(transform)) { WinLevel(); print("win level 2"); }
         //transform.position = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z);
         //transform.eulerAngles = initialRotation;
-        girl.transform.position = initialPosition;
-        dickButt.transform.position = initialPosition;
-        girl.transform.eulerAngles = initialRotation;
-        dickButt.transform.eulerAngles = initialRotation;
+		femaleCharacter.transform.position = initialPosition;
+		maleCharacter.transform.position = initialPosition;
+		femaleCharacter.transform.eulerAngles = initialRotation;
+		maleCharacter.transform.eulerAngles = initialRotation;
 
     }
 
